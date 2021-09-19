6a08203b1e4acaf11885a24fff655238af48aabf
Stonzyy <33039121+Stonzyy@users.noreply.github.com>
2021-09-20 02:27:03 +0300

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+while getopts "u:" arg; do
+  case $arg in
+    u) site=$OPTARG;;
+  esac
+done
+
+subfinder -d $site > subs.txt; cat subs.txt | httpx --silent > valid.txt; cat valid.txt | waybackurls > way.txt; cat way.txt | kxss
