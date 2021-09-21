3ece2441c0fabb2b26965dd6010311f813e3b0d8
Stonzyy <33039121+Stonzyy@users.noreply.github.com>
2021-09-21 20:04:25 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -6,4 +6,7 @@ while getopts "u:" arg; do
   esac
 done
 
-subfinder -d $site > subs.txt; cat subs.txt | httpx --silent > valid.txt; cat valid.txt | waybackurls > way.txt; cat way.txt | grep = > params.txt; cat params.txt | kxss
+subfinder -d $site > subs.txt; cat subs.txt | httpx --silent > valid.txt; cat valid.txt | waybackurls > way.txt; 
+cat way.txt | grep = > params.txt
+
+cat params.txt | kxss
